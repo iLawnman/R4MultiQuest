53e9d77aec0f8b74fda71e739af0296cc8236368
Ilia Samovichev <i9091780@gmail.com>
2024-12-28 21:40:33 +0545

    googlesheet request all links from page

--- Assets/_R4Quest/Scripts/GoogleSheets/GoogleSheetMono.cs
+++ Assets/_R4Quest/Scripts/GoogleSheets/GoogleSheetMono.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
 using Newtonsoft.Json;
 using UnityEngine;
 
@@ -17,81 +18,37 @@ namespace _R4Quest.Scripts.GoogleSheets
         [SerializeField] private string spreadsheetId;
         [SerializeField] private string sheetName;
 
-        void OnEnable()
-        {
-            LoadImages();
-        }
-
         [ContextMenu("DO")]
         public void Do()
         {
-            //LoadImages();
             FetchImagesFromGoogleSheet();
         }
 
-        async Task FetchImagesFromGoogleSheet()
+        async UniTask<List<string>> FetchImagesFromGoogleSheet()
         {
-            //https://script.google.com/macros/s/AKfycbzNBMSd4FzfaSPr389vnHsKTivDyv1Ow64ilmrE_zSDS9WjLy3nd3t4xBFOeKZcoRM-0A/exec
             var scriptUrl =
-                "https://script.google.com/macros/s/AKfycbzNBMSd4FzfaSPr389vnHsKTivDyv1Ow64ilmrE_zSDS9WjLy3nd3t4xBFOeKZcoRM-0A/exec";
-            //string scriptUrl = "https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec";
+                "https://script.google.com/macros/s/AKfycbxBpEA7jspwLve_y6RaV6wU54SOUlhGHMS_SjQDkVaiE-JBlb7vExYrlRkSumw3YHED2w/exec";
+                //"https://script.google.com/macros/s/AKfycbzNBMSd4FzfaSPr389vnHsKTivDyv1Ow64ilmrE_zSDS9WjLy3nd3t4xBFOeKZcoRM-0A/exec";
         
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(scriptUrl);
             request.Method = "GET";
             request.ContentType = "application/json";
-
             try
             {
                 using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
                 using (StreamReader reader = new StreamReader(response.GetResponseStream()))
                 {
-                    string result = reader.ReadToEnd();
-                    //Debug.Log("resBody " +result);
-                    List<string> imageLinks = JsonConvert.DeserializeObject<List<string>>(result);
-                    
-                    foreach (var link in imageLinks)
-                    {
-                        Debug.Log("link " +link);
-                    }
+                    string result = await reader.ReadToEndAsync();
+                    List<string> links = JsonConvert.DeserializeObject<List<string>>(result);
+                    Debug.Log(links.Count);
+                    return links;
                 }
             }
             catch (Exception e)
             {
                 Debug.LogError($"Ошибка: {e.Message}");
             }
-        }
-        private void LoadImages()
-        {
-            credentialsPath = Path.Combine(Application.dataPath, "Resources/credentials.json");
-
-            var service = new GoogleSheetsService(credentialsPath);
-            var imageLinks = service.GetImageLinks(spreadsheetId, sheetName);
-
-            foreach (var link in imageLinks)
-            {
-                Debug.Log($"Image URL: {link}");
-                StartCoroutine(DownloadAndDisplayImage(link));
-            }
-        }
-
-        private IEnumerator DownloadAndDisplayImage(string url)
-        {
-            using var www = new UnityEngine.Networking.UnityWebRequest(url);
-            www.downloadHandler = new UnityEngine.Networking.DownloadHandlerTexture();
-
-            yield return www.SendWebRequest();
-
-            if (www.result == UnityEngine.Networking.UnityWebRequest.Result.Success)
-            {
-                Texture2D texture = ((UnityEngine.Networking.DownloadHandlerTexture)www.downloadHandler).texture;
-                // Здесь вы можете применить текстуру к объекту
-                Debug.Log($"Image downloaded: {url}");
-            }
-            else
-            {
-                Debug.LogError($"Failed to download image: {url}, Error: {www.error}");
-            }
+            return null;
         }
     }
-
 }
--- Assets/_R4Quest/Scripts/GoogleSheets/GoogleSheetService.cs
+++ Assets/_R4Quest/Scripts/GoogleSheets/GoogleSheetService.cs
@@ -1,87 +1,35 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Threading;
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.Drive.v3;
-using Google.Apis.Services;
-using Google.Apis.Sheets.v4;
-using Google.Apis.Util.Store;
+using System.Net;
+using Cysharp.Threading.Tasks;
+using Newtonsoft.Json;
 using UnityEngine;
 
 public class GoogleSheetsService
 {
-    //https://script.google.com/macros/s/AKfycbw16DHsypmU4oIe25yNugWyCYhgpG3GxNBebTXdpyzYLja5tb6JuwBTHFkLwJ84SJTArA/exec
-    
-    private const string ApplicationName = "Unity Google Sheets Integration";
-    private SheetsService sheetsService;
-    private DriveService driveService;
-    
-    public GoogleSheetsService(string credentialsPath)
+    async UniTask<List<string>> FetchLinksFromGoogleSheet()
     {
-        //var credential = Authenticate(credentialsPath);
-        var credential = AuthenticateServiceAccount(credentialsPath);
-        sheetsService = new SheetsService(new BaseClientService.Initializer
+        var scriptUrl =
+            "https://script.google.com/macros/s/AKfycbxBpEA7jspwLve_y6RaV6wU54SOUlhGHMS_SjQDkVaiE-JBlb7vExYrlRkSumw3YHED2w/exec";
+        
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(scriptUrl);
+        request.Method = "GET";
+        request.ContentType = "application/json";
+        try
         {
-            HttpClientInitializer = credential,
-            ApplicationName = ApplicationName
-        });
-    
-        driveService = new DriveService(new BaseClientService.Initializer
-        {
-            HttpClientInitializer = credential,
-            ApplicationName = ApplicationName
-        });
-    }
-    
-    private ServiceAccountCredential AuthenticateServiceAccount(string credentialsPath)
-    {
-        using var stream = new FileStream(credentialsPath, FileMode.Open, FileAccess.Read);
-        var credential = GoogleCredential.FromStream(stream)
-            .CreateScoped(SheetsService.Scope.SpreadsheetsReadonly, DriveService.Scope.DriveReadonly)
-            .UnderlyingCredential as ServiceAccountCredential;
-
-        return credential;
-    }
-    private UserCredential Authenticate(string credentialsPath)
-    {
-        using var stream = new FileStream(credentialsPath, FileMode.Open, FileAccess.Read);
-        //string credPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), ".credentials/sheets.googleapis.com.json");
-        string credPath = Path.Combine(Application.persistentDataPath, ".credentials/sheets.googleapis.com.json");
-    
-        return GoogleWebAuthorizationBroker.AuthorizeAsync(
-            GoogleClientSecrets.Load(stream).Secrets,
-            new[] { SheetsService.Scope.SpreadsheetsReadonly, DriveService.Scope.DriveReadonly },
-            "user",
-            CancellationToken.None,
-            new FileDataStore(credPath, true)).Result;
-    }
-    
-    public List<string> GetImageLinks(string spreadsheetId, string sheetName)
-    {
-        Debug.Log("get image links " + spreadsheetId + " " + sheetName);
-
-        //var request = sheetsService.Spreadsheets.Values.Get(spreadsheetId, sheetName);
-        var request = sheetsService.Spreadsheets.Values.Get(spreadsheetId, sheetName);
-        var response = request.Execute();
-        Debug.Log("responce " + response.Values);
-
-        var links = new List<string>();
-        foreach (var row in response.Values)
-        {
-            foreach (var cell in row)
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
             {
-                if(Uri.IsWellFormedUriString(cell.ToString(), UriKind.RelativeOrAbsolute))
-                    Debug.Log("cell " + cell.GetType() + " / " + cell);
-
-                // if (cell is string cellValue && Uri.IsWellFormedUriString(cellValue, UriKind.Absolute))
-                // {
-                //     Debug.Log("add " + cellValue);
-                //     links.Add(cellValue);
-                // }
+                string result = await reader.ReadToEndAsync();
+                List<string> links = JsonConvert.DeserializeObject<List<string>>(result);
+                return links;
             }
         }
-    
-        return links;
+        catch (Exception e)
+        {
+            Debug.LogError($"Ошибка: {e.Message}");
+        }
+        return null;
     }
 }
